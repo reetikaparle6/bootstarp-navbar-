0cd4224be81bfd8ead6a8ce56ec7ac6232072fd5
Reetika <hireetika@gmail.com>
2021-07-01 12:17:41 +0530

    Initial commit

---
+++ enroll/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from enroll.models import Student
+
+# Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('studid','stuname')
+
+admin.site.register(Student,StudentAdmin)
---
+++ enroll/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from enroll.models import Student
+
+# Create your views here.
+
+def studentinfo(request):
+    stud=Student.objects.all()
+    return render(request,"enroll/studetails.htm",{'stu':stud})
